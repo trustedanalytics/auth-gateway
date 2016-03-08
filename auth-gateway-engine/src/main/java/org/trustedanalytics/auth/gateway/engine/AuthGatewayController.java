e0a29f9a70c3ba606d5459b07beafb6be3ab6c15
mraczyns <marta.raczynska@intel.com>
2016-03-08 14:04:27 +0100

    Adding API documentation

--- auth-gateway-engine/src/main/java/org/trustedanalytics/auth/gateway/engine/AuthGatewayController.java
+++ auth-gateway-engine/src/main/java/org/trustedanalytics/auth/gateway/engine/AuthGatewayController.java
@@ -13,6 +13,7 @@
  */
 package org.trustedanalytics.auth.gateway.engine;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,12 +37,15 @@ class AuthGatewayController {
         authGatewayEngine.addUser(userId);
     }
 
+    @ApiOperation("Creating organization: in case of hdfs - creating directory, zookeeper - creating znode, " +
+            "sentry - roles on cdh cluster, hgm - usergroupmapping")
     @RequestMapping(value = "/organizations/{orgId}", method = RequestMethod.PUT)
     public void addOrganization(@PathVariable String orgId)
             throws AuthorizableGatewayException {
         authGatewayEngine.addOrganization(orgId);
     }
 
+    @ApiOperation("Adding user's access to hadoop components in given organization")
     @RequestMapping(value = "/organizations/{orgId}/users/{userId}", method = RequestMethod.PUT)
     public void addUserToOrganization(@PathVariable String userId, @PathVariable String orgId)
             throws AuthorizableGatewayException {
@@ -54,12 +58,15 @@ class AuthGatewayController {
         authGatewayEngine.removeUser(userId);
     }
 
+    @ApiOperation("Removing organization: in case of hdfs - removing directory, zookeeper - removing znode, " +
+            "sentry - roles on cdh cluster, hgm - usergroupmapping")
     @RequestMapping(value = "/organizations/{orgId}", method = RequestMethod.DELETE)
     public void deleteOrganization(@PathVariable String orgId)
             throws AuthorizableGatewayException {
         authGatewayEngine.removeOrganization(orgId);
     }
 
+    @ApiOperation("Removing user's access to hadoop components in given organization")
     @RequestMapping(value = "/organizations/{orgId}/users/{userId}", method = RequestMethod.DELETE)
     public void deleteUserFromOrganization(@PathVariable String userId, @PathVariable String orgId)
             throws AuthorizableGatewayException {
---
+++ auth-gateway-engine/src/main/java/org/trustedanalytics/auth/gateway/engine/SwaggerConfig.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2015 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.trustedanalytics.auth.gateway.engine;
+
+import static com.google.common.base.Predicates.or;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+import com.google.common.base.Predicate;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    Docket orgsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .ignoredParameterTypes(Authentication.class)
+                .apiInfo(apiInfo())
+                .select()
+                .paths(orgsPaths())
+                .build()
+                .useDefaultResponseMessages(false);
+    }
+
+    private Predicate<String> orgsPaths() {
+        return or(regex("/organizations/.*"), regex("/users/.*"));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Auth-gateway API")
+                .description("Api endpoints for adding and removing authorization data for hadoop components in TAP")
+                .license("Apache License Version 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
+                .build();
+    }
+}
